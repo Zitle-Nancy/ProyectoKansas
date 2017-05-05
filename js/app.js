a2d014de4ec5c6d11a1dfa6ce7dc7c3d6118e173
nancyzj <ancizj@hotmail.com>
2017-05-05 14:37:11 -0500

    version clases

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
-function sombrear(celda){
-	celda.style.backgroundColor = "#ef3b9e";
+// function sombrear(celda){
+// 	celda.style.backgroundColor = "#ef3b9e";
 
+// }
+var letra = document.getElementsByClassName('letra');
+var mexico = document.getElementsByClassName('mexico');
+var arregloPalabras = [];
+var longitud = letra.length;
+
+// var th = document.getElementsByTagName('th');
+for(var j=0; j < mexico.length; j++){
+	// console.log(mexico[j].innerHTML);
+	arregloPalabras.push(mexico[j].innerHTML);
+
+}
+console.log(arregloPalabras);
+if (arregloPalabras.join("") == "MEXICO"){
+	alert("bien");
+}else{
+	alert("mal");
+}
+// console.log(th);
+
+for(var i=0; i< longitud; i++){
+	letra[i].addEventListener('click',pintar);
+}
+
+function pintar(){
+	this.style.backgroundColor = "red";
 }
