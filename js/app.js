0bfa223e2af7e3934673cfa37c1853c51f7c2c87
sue martinez <sueherrera30@gmail.com>
2017-05-05 15:03:31 +0000

    inicio del juego con boton

--- js/app.js
+++ js/app.js
@@ -2,3 +2,15 @@ function sombrear(celda){
 	celda.style.backgroundColor = "#ef3b9e";
 
 }
+
+
+// FUNCIONALIDAD DEL BOTON PARA INICIAR EL JUEGO
+var botonInicio = document.getElementsByClassName("btn")[0];
+console.log(botonInicio);
+
+botonInicio.addEventListener("click", mostrarJuego);
+
+function mostrarJuego() {
+	var contenedor = document.getElementById("contenedor-chido");
+	contenedor.style.visibility = "visible";
+}
